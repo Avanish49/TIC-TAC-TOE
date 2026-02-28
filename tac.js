6a5537add7c9b2d9b35734d7776a090928909e49
Avanish49 <avanishkhichi071@gmail.com>
2026-02-28 12:06:14 +0530

    js

---
+++ tac.js
@@ -0,0 +1,76 @@
+let boxes = document.querySelectorAll(".box");
+let reset = document.querySelector(".reset");
+let newgamebtn = document.querySelector(".new");
+let para = document.querySelector("p");
+let cont = document.querySelector(".cont");
+
+let turnO = true;
+let winpattern = [
+    [0,1,2],
+    [0,4,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+const resetgame = ()=>{
+    boxenabled();
+    turnO = true;
+    cont.classList.add("cont");
+
+}
+const boxenabled = ()=>{
+ for(let box of boxes){
+    box.disabled = false;
+    box.innerText = "";
+    }
+}
+newgamebtn.addEventListener("click",resetgame);
+reset.addEventListener("click",resetgame);
+
+boxes.forEach((box) => {
+    box.addEventListener("click",() => {
+        console.log("button was clicked");
+        if(turnO){
+            box.innerText = "O";
+            turnO =false;
+        }
+        else {
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+        const checkpattern = ()=>{
+    for(let pattern of winpattern){
+        
+        let pos1 = boxes[pattern[0]].innerText;
+        let pos2 = boxes[pattern[1]].innerText;
+        let pos3 = boxes[pattern[2]].innerText;
+       if(pos1!=="" && pos2!=="" && pos3!==""){
+        if(pos1 == pos2 && pos2==pos3){
+            console.log("winner", pos1);
+            showwinner = (winner)=>{
+                para.innerText = `winner is ${winner}`;
+                cont.classList.remove("cont");
+                const boxdisabled = ()=>{
+                    for(let box of boxes){
+                        box.disabled = true;
+                    }
+                }
+               boxdisabled();
+            }
+            showwinner(pos1);
+             
+
+        }
+       }
+
+    }
+
+}
+        checkpattern();
+    })
+    
+})
